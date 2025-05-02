9e8aead0282db435dd0752f6dd7ccc1c3c6c2ead
Josue IRIRWANIRIRA <ijjoshua200@gmail.com>
2025-05-02 14:40:03 +0200

    Add files via upload

---
+++ .github/workflows/pull and run.sh
@@ -0,0 +1,8 @@
+docker pull yourusername/mission-management
+docker run -it yourusername/mission-management
+
+docker pull yourusername/land-management
+docker run -it yourusername/land-management
+
+docker pull yourusername/nursery-school
+docker run -it yourusername/nursery-school
